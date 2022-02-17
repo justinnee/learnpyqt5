6737f289f051fcd5b0144daef3505a3a1c6980aa
justinnee <1442044931@qq.com>
2022-02-17 13:41:55 +0800

    CustomSignal.py Multi argv

--- src/SignalSlot/CustomSignal.py
+++ src/SignalSlot/CustomSignal.py
@@ -1,5 +1,5 @@
 '''
-
+Lesson 106-107
 自定义信号
 pyqtSignal()
 
@@ -12,12 +12,22 @@ class MyTypeSiganl(QObject):
     # 定义一个信号
     sendmsdg = pyqtSignal(object)
 
+    # 发送3个参数的信号
+    sendmsg1 = pyqtSignal(str,int,int)
+
     def run(self):
         self.sendmsdg.emit('Hello PyQt5')
+
+    def run1(self):
+        self.sendmsg1.emit('Hello',3,4)
+
 
 class MySlot(QObject):
     def get(self,msg):
         print('信息:'+msg)
+    def get1(self,msg,a,b):
+        print(msg)
+        print(a+b)
 
 
 if __name__ == '__main__':
@@ -25,6 +35,10 @@ if __name__ == '__main__':
     slot = MySlot()
 
     send.sendmsdg.connect(slot.get)
+    send.sendmsg1.connect(slot.get1)
+    send.run()
+    send.run1()
 
+    send.sendmsdg.disconnect(slot.get)
     send.run()
 
